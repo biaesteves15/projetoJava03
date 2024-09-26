750e7ca4ee1ce422f52d65ed06cf6027b2588232
beatriz.esteves <biaoliveirasteves@gmail.com>
2024-09-26 14:26:37 -0300

    first commit

---
+++ src/factories/ConnectionFactory.java
@@ -0,0 +1,21 @@
+package factories;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionFactory {
+
+	public static Connection getConnection() {
+		Connection connection = null; // vazio
+		try {
+			Class.forName("org.postgresql.Driver");
+			connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/bd_aula02", "postgres", "2234");
+
+		} catch (Exception e) {
+			System.out.println("\nFalha ao abrir conexão com o banco de dados.");
+			System.out.println(e.getMessage());
+		}
+		return connection;
+	}
+
+}
